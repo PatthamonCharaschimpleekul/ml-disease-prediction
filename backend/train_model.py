f44eb0c9e94c10156fa7e3754230d16b7ebb8304
Toroko <patthamon.chr@gmail.com>
2026-03-13 16:48:18 +0800

    check data

---
+++ backend/train_model.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+#download Dataset
+data = pd.read_csv('data/Training.csv')
+
+#data observes
+'''print("\n--------DATASET INFO--------")
+print(data.info)
+print("dataset shape: ", data.shape)
+print(data.head)
+
+missing = data.isnull().sum()
+print(missing[missing > 0])
+data = data.dropna()
+
+duplicate_rows = data.duplicated().sum()
+print("Duplicate rows:", duplicate_rows)
+data = data.drop_duplicates()
+
+print("\n----- DATA TYPES -----")
+print(data.dtypes)
+
+print("\n------ CHECK INVALID VALUES -----")
+symptom_columns = data.columns[:-1]
+for col in symptom_columns:
+    unique_vals = data[col].unique()
+    if not set(unique_vals).issubset({0,1}):
+        print(f"Column {col} has invalid values:", unique_vals)
+
+print("\n----- UNIQUE PROGNOSIS NAMES ------")
+print(data["prognosis"].unique())'''
+
+X = data.drop("prognosis", axis=1)
+y = data["prognosis"]
+symptom_list = list(X.columns)
