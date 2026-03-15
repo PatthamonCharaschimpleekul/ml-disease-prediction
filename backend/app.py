c0f01e0e08b6eaaf91ea522b2f80912cb18f7260
Toroko <patthamon.chr@gmail.com>
2026-03-15 21:41:59 +0800

    add dropdown

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,29 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, render_template, request
 from predict import predict_disease
+import pickle
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET", "POST"])
+symptom_list = pickle.load(open("symptom_list.pkl", "rb"))
+
+@app.route("/", methods=["GET", "POST"])
 def home():
+
     predictions = None
     symptoms_input = ""
 
     if request.method == "POST":
+
         symptoms_input = request.form["symptoms"]
         symptoms = [s.strip() for s in symptoms_input.split(",")]
+
         predictions = predict_disease(symptoms)
+
     return render_template(
-        "index.html", predictions=predictions, symptoms_input=symptoms_input
+        "index.html",
+        predictions=predictions,
+        symptoms_input=symptoms_input,
+        symptom_list=symptom_list
     )
 
 if __name__ == "__main__":
