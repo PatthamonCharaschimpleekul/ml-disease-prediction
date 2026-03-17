77c24040458a71bd268815dffbb5327dc463276d
Toroko <patthamon.chr@gmail.com>
2026-03-17 21:05:35 +0800

    evaluate deduplication

--- backend/app.py
+++ backend/app.py
@@ -5,24 +5,27 @@ import pickle
 app = Flask(__name__)
 
 symptom_list = pickle.load(open("symptom_list.pkl", "rb"))
-
 display_symptoms = [s.replace("_"," ") for s in symptom_list]
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-
     predictions = None
     symptoms_input = ""
 
     if request.method == "POST":
+        # รับค่าจาก Textbox
+        s_input = request.form.get("symptoms", "")
+        symptoms_text = [s.strip() for s in s_input.split(",") if s.strip()]
 
-        symptoms_input = request.form["symptoms"]
-        symptoms = [s.strip() for s in symptoms_input.split(",")]
-
+        # รับค่าจาก Checkbox
         checkbox_symptoms = request.form.getlist("symptom_checkbox")
-        symptoms.extend(checkbox_symptoms)
 
-        predictions = predict_disease(symptoms)
+        # รวมค่าและกำจัดตัวซ้ำ (Set)
+        final_symptoms = list(set(symptoms_text + checkbox_symptoms))
+
+        if final_symptoms:
+            predictions = predict_disease(final_symptoms)
+            symptoms_input = ", ".join(final_symptoms)
 
     return render_template(
         "index.html",
